ace7d88c79ad702ce2da847f1592f93d6fe1c834
saaratrix <saaratrix@gmail.com>
2026-02-16 00:11:28 +0200

    Can upload file and save it to server, also in previous commit increased upload limit to 100 GB.

--- nuucast/src/uploader.rs
+++ nuucast/src/uploader.rs
@@ -1,13 +1,23 @@
+use std::path::PathBuf;
 use axum::{
     extract::Path as AxumPath,
     body::Bytes,
     http::StatusCode,
 };
+use crate::io::file_utility::{get_url_and_filepath_from_url, UrlAndFilePath, MEDIA_ROOT};
 
 pub async fn handle_upload(
-    path: Option<AxumPath<String>>,
+    AxumPath(url) : AxumPath<String>,
     body: Bytes,
 ) -> StatusCode {
+    let paths = get_url_and_filepath_from_url(&url).unwrap_or_else(|| UrlAndFilePath { url: PathBuf::new(), filepath: MEDIA_ROOT.clone() });
 
-    return StatusCode::OK;
+    match tokio::fs::write(&paths.filepath, &body).await {
+        Ok(_) => {
+            StatusCode::OK
+        }
+        Err(e) => {
+            StatusCode::INTERNAL_SERVER_ERROR
+        }
+    }
 }
